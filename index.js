27f5ca3083a97326a4107ce950dba7ed6cfa2fab
picasso <929144387@qq.com>
2024-03-04 14:33:19 +0800

    更新函数

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 // 随机数生成器
 export function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -22,4 +24,13 @@ export function parseURLParams(url) {
 // 字符串逆转函数
 export function reverseString(str) {
   return str.split('').reverse().join('');
+}
+// 日期检测 检测当前日期是否在 date 之后
+export function isAfter(currentTime, compareTime) {
+  // 使用 Day.js 解析时间
+  const current = dayjs(currentTime);
+  const compare = dayjs(compareTime);
+  
+  // 判断当前时间是否在比较时间之后
+  return current.isAfter(compare);
 }
